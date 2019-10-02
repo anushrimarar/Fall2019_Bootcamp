d24cff991c7241c00a7a7a6671fef631975a10fd
Anushri Marar <32146414+anushrimarar@users.noreply.github.com>
2019-10-01 23:38:08 -0400

    Add files via upload
    Bootcamp 3 Commit

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    db: {
+      uri: 'mongodb+srv://guest2:BUAufI9wyqXegw3O@cluster0-2q2gm.mongodb.net/test?retryWrites=true&w=majority'//place the URI of your mongo database here.
+    }, 
+    openCage: {
+      key: 'dac4603b644d4830afdec090a67b59eb' //place your openCage public key here - Sign-up for a free key https://opencagedata.com/
+    },
+    port: 8080
+  };
